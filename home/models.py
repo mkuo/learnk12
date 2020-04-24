b607f01283f335b5fd2e44d70da7c70315ef91cd
Mike Kuo <mcykuo@gmail.com>
2020-04-24 12:59:48 -0700

    Add courses

--- home/models.py
+++ home/models.py
@@ -4,4 +4,15 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['courses'] = Page.objects.type(CourseDetailPage)
+        return context
+
+
+class CoursesPage(Page):
+    pass
+
+
+class CourseDetailPage(Page):
     pass
