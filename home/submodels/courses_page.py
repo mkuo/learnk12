21fc34f3f62dac6f27bb08de2a8ca6f34f20abe2
Mike Kuo <mcykuo@gmail.com>
2020-04-29 19:12:56 -0700

    Add tag filter

--- home/submodels/courses_page.py
+++ home/submodels/courses_page.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator, EmptyPage
 from django.db.models import Q
+from taggit.models import Tag
 from wagtail.core.models import Page
 
 from home.submodels.course_detail_page import CourseDetailPage
@@ -49,11 +50,16 @@ class CoursesPage(Page):
             sort_button_styling[col_key]['button_color'] = 'btn-primary'
         return sort_button_styling
 
-    def _get_courses_paged(self, page, sort_args, difficulty_args, provider_args):
+    def _get_courses_paged(self, page, sort_args, tag_args, difficulty_args, provider_args):
         # get courses from database
         if not sort_args:
             sort_args = [self.default_sort]
         course_query = CourseDetailPage.objects.live().order_by(*sort_args)
+
+        tag_filter = Q()
+        for tag in tag_args:
+            tag_filter |= Q(tags__id=tag)
+        course_query = course_query.filter(tag_filter).distinct()
 
         difficulty_filter = Q()
         for difficulty in difficulty_args:
@@ -86,22 +92,33 @@ class CoursesPage(Page):
             'num_courses': paginator.count
         }
 
+    def _get_tags(self, request):
+        results = Tag.objects.values('id', 'name')
+        tags = [(res['id'], res['name']) for res in results]
+        tag_args = self._sanitize_args(request, 'tag', tags)
+        return tag_args, tags
+
     def _get_difficulties(self, request):
-        difficulties = [enum_val for enum_val, _ in CourseDetailPage.CourseDifficulty.choices]
+        difficulties = CourseDetailPage.CourseDifficulty.choices
         difficulty_args = self._sanitize_args(request, 'difficulty', difficulties)
         return difficulty_args, difficulties
 
     def _get_providers(self, request):
         results = CourseDetailPage.objects.live().order_by().values('provider').distinct()
-        providers = [result['provider'] for result in results]
+        providers = [(res['provider'], res['provider']) for res in results]
         provider_args = self._sanitize_args(request, 'provider', providers)
         return provider_args, providers
 
     @staticmethod
     def _get_filter_buttons(args, choices):
-        dropdown_btns = {str(choice): 'badge-white' for choice in choices}
+        dropdown_btns = {
+            str(choice[0]): {
+                'label': choice[1],
+                'btn_color': 'badge-white'
+            } for choice in choices
+        }
         for selection in args:
-            dropdown_btns[selection] = 'badge-primary'
+            dropdown_btns[selection]['btn_color'] = 'badge-primary'
         if args:
             main_btn = 'btn-primary'
         else:
@@ -118,14 +135,18 @@ class CoursesPage(Page):
         sort_args = self._sanitize_args(request, 'sort', self.sort_columns)
         context['sort_buttons'] = self._get_sort_buttons(sort_args)
 
+        tag_args, tags = self._get_tags(request)
         difficulty_args, difficulties = self._get_difficulties(request)
         provider_args, providers = self._get_providers(request)
         context['filter_labels'] = {
+            'tag': self._get_filter_buttons(tag_args, tags),
             'difficulty': self._get_filter_buttons(difficulty_args, difficulties),
             'provider': self._get_filter_buttons(provider_args, providers)
         }
 
         page = self._sanitize_page(request)
-        context['courses_paged'] = self._get_courses_paged(page, sort_args, difficulty_args, provider_args)
+        context['courses_paged'] = self._get_courses_paged(
+            page, sort_args, tag_args, difficulty_args, provider_args
+        )
 
         return context
--- home/templatetags/model_tags.py
+++ home/templatetags/model_tags.py
@@ -4,6 +4,7 @@ from home.submodels.course_detail_page import CourseDetailPage
 
 register = Library()
 
+
 @register.filter
 def course_difficulty_enum(text):
     return CourseDetailPage.CourseDifficulty(int(text)).label
