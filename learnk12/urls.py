eed7b2fa11a9c545cf3d664cff0a243e13a37ac7
Mike Kuo <mcykuo@gmail.com>
2020-05-13 21:30:02 -0700

    Add sitemap

--- learnk12/urls.py
+++ learnk12/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
@@ -13,11 +14,10 @@ urlpatterns = [
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'^sitemap\.xml$', sitemap),
 
     url(r'^search/$', search_views.search, name='search'),
-
 ]
-
 
 if settings.DEBUG:
     from django.conf.urls.static import static
