35ddce10a95993254820d8d90f4d06f68a10bdc0
Mike Kuo <mcykuo@gmail.com>
2020-05-22 16:14:06 -0700

    Update courses and home page to use table

--- home/forms/widgets.py
+++ home/forms/widgets.py
@@ -2,4 +2,4 @@ from django.forms.widgets import ChoiceWidget
 
 
 class StarRatingWidget(ChoiceWidget):
-    template_name = 'home/star_rating.html'
+    template_name = 'home/star_rating_form.html'
