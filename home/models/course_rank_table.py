57ff21750a804c87a59570756f0519cae3f2675b
Mike Kuo <mcykuo@gmail.com>
2020-05-25 22:44:37 -0700

    Add stream field to home page

---
+++ home/models/course_rank_table.py
@@ -0,0 +1,34 @@
+from wagtail.core import blocks
+
+from home.models import CourseDetailPage
+
+
+class CourseRankTableBlock(blocks.StructBlock):
+    heading = blocks.CharBlock(required=False)
+    caption = blocks.CharBlock(required=False)
+    filters = blocks.ListBlock(blocks.StructBlock([
+        ('column', blocks.CharBlock()),
+        ('value', blocks.CharBlock()),
+    ]))
+    excludes = blocks.ListBlock(blocks.StructBlock([
+        ('column', blocks.CharBlock()),
+        ('value', blocks.CharBlock()),
+    ]))
+    sorts = blocks.ListBlock(blocks.CharBlock())
+    result_count = blocks.IntegerBlock()
+
+    class Meta:
+        template = 'home/course_rank_table.html'
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        course_query = CourseDetailPage.objects.live().public()
+        if value['filters']:
+            kwargs = {f['column']: f['value'] for f in value['filters']}
+            course_query = course_query.filter(**kwargs)
+        for e in value['excludes']:
+            course_query = course_query.exclude(**{e['column']: e['value']})
+        if value['sorts']:
+            course_query = course_query.order_by(*value['sorts'])
+        context['courses'] = course_query[:value['result_count']]
+        return context
--- home/models/home_page.py
+++ home/models/home_page.py
@@ -1,16 +1,23 @@
+from django.db import models
+from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 
-from home.models.course_detail_page import CourseDetailPage
+from home.models.course_rank_table import CourseRankTableBlock
 
 
 class HomePage(Page):
     max_count = 1
     parent_page_type = ['Page']
     subpage_types = ['CoursesPage', 'TutorsPage', 'InfoPage']
+    heading = models.CharField(max_length=255)
+    caption = models.TextField(blank=True, null=True)
+    course_rank_tables = StreamField([
+        ('course_rank_table', CourseRankTableBlock())
+    ], blank=True, null=True)
 
-    def get_context(self, request):
-        context = super().get_context(request)
-        course_query = CourseDetailPage.objects.live().public()
-        context['best_free'] = course_query.filter(cost=0).order_by('avg_score')[:3]
-        context['highest_rated'] = course_query.order_by('avg_score')[:3]
-        return context
+    content_panels = Page.content_panels + [
+        FieldPanel('heading'),
+        FieldPanel('caption'),
+        StreamFieldPanel('course_rank_tables'),
+    ]
