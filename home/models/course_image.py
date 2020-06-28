d0a4dc4a26ddf0bde688067ded2e927aa9892e2a
Mike Kuo <mcykuo@gmail.com>
2020-06-28 14:28:44 -0700

    Set image focal points automatically

--- home/models/course_image.py
+++ home/models/course_image.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.models import Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.models import Image
+from wagtail.images.rect import Rect
 
 from home.models.course_page import CoursePage
 
@@ -26,3 +30,11 @@ class CourseImage(Orderable):
         ImageChooserPanel("image"),
         FieldPanel("caption")
     ]
+
+
+@receiver(pre_save, sender=Image, dispatch_uid="pre_save_image_focus")
+def pre_save_image_focus(sender, instance, **kwargs):
+    if instance.focal_point_x is None and instance.focal_point_y is None:
+        right = min(instance.width, 64)
+        bottom = min(instance.height, 64)
+        instance.set_focal_point(Rect(0, 0, right, bottom))
