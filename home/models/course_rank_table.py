042cd27e3f9814a5b463bf7460baf2228d1f3a98
Mike Kuo <mcykuo@gmail.com>
2020-06-28 19:42:05 -0700

    Require page choice for course_rank_table

--- home/models/course_rank_table.py
+++ home/models/course_rank_table.py
@@ -7,7 +7,7 @@ class CourseRankTableBlock(blocks.StructBlock):
     heading = blocks.CharBlock(required=False)
     link = blocks.PageChooserBlock(required=False)
     caption = blocks.CharBlock(required=False)
-    courses = blocks.ListBlock(blocks.PageChooserBlock(page_type=CoursePage))
+    courses = blocks.ListBlock(blocks.PageChooserBlock(page_type=CoursePage, required=True))
 
     class Meta:
         template = 'home/course_rank_table.html'
