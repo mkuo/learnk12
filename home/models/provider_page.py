3c2e1b5cbcfab82a6de9f9246573f2f7d03a3a54
Mike Kuo <mcykuo@gmail.com>
2020-07-05 09:55:50 -0700

    Add provider logo

--- home/models/provider_page.py
+++ home/models/provider_page.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class ProviderPage(Page):
@@ -10,8 +11,16 @@ class ProviderPage(Page):
 
     description = RichTextField()
     provider_url = models.URLField(blank=True, null=True)
+    logo = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        related_name="+",
+        on_delete=models.SET_NULL
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel('description'),
         FieldPanel('provider_url'),
+        ImageChooserPanel('logo'),
     ]
