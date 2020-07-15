958c7d2182407a1d94625533637bad5db947555e
onixdevgmail <onix.develop@gmail.com>
2020-07-15 22:36:44 +0300

    Auth fix

--- user_auth/models.py
+++ user_auth/models.py
@@ -4,6 +4,10 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
+
+    class Meta:
+        db_table = 'auth_user'
+
     birth_date = models.DateField(null=True, blank=False)
     photo = models.ImageField(upload_to='images/profile/', null=False, blank=True)
     email = models.EmailField(_('email address'), unique=True)
